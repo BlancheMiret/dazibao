fdb953f788b059f8c6e20ff4e584c3fdc337430a
CHEKROUN hiba-asmaa <chekrouh@informatique.univ-paris-diderot.fr>
2020-03-19 21:43:11 +0100

    correction

--- tlv.c
+++ tlv.c
@@ -1,4 +1,12 @@
-#include "tlvFct.h"
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <inttypes.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <net/if.h>
 #define MSG_HEADER 4
 #define TLV_HEADER 2
 
@@ -17,6 +25,33 @@ int main_datagram(char * msg){
 
 
 }
+
+
+/****** longueur du message principal *******/
+
+uint16_t length_body(char *messageTLV)
+{
+    uint16_t length;
+    memcpy(&length, messageTLV + 2, 2);
+    uint16_t len = ntohs(length);
+    return len;
+}
+
+int msgBodyTosend(char *message, char *body, uint16_t len)
+{
+
+    uint16_t convert_len = ntohs(length_body(message));
+
+    memcpy(message + 4 + convert_len, body, len);
+
+    convert_len = convert_len + len;
+
+    uint16_t convert_h = htons(convert_len);
+    memcpy(message + 2, &convert_h, 2);
+
+    return MSG_HEADER + convert_len;
+}
+
 
 
 //TLV
@@ -97,12 +132,12 @@ int Network_hash(char * network_hash, char * hash){
 	uint8_t type = 0x4;
 
  	uint8_t len = 0x0;
-	memcpy(network_req, &type, 1);
-	memcpy(network_req+1, &len, 1);
+	memcpy(network_hash, &type, 1);
+	memcpy(network_hash+1, &len, 1);
 	return len+TLV_HEADER;
 
 
-}t 
+}
 
 //Creation de Network State Request
 
@@ -122,7 +157,7 @@ int Network_state_request(char * network_req){
 
 //Creation de Node Hash
 
-int Node_hash(char * node_hash, uint64_t node_id, uint16_t seqno, char * node_hash){
+int Node_hash(char * node_hash, uint64_t node_id, uint16_t seqno, char * hash){
     memset(node_hash, 0, SIZE-MSG_HEADER);
 
 	uint8_t type = 0x6;
@@ -132,7 +167,7 @@ int Node_hash(char * node_hash, uint64_t node_id, uint16_t seqno, char * node_ha
 	memcpy(node_hash+1, &len, 1);
 	memcpy(node_hash+2, &node_id, 8);
 	memcpy(node_hash+10, &seqno, 2);
-	memcpy(node_hash+12, &seqno, 16);
+	memcpy(node_hash+12, &hash, 16);
 	return len+TLV_HEADER;
 
 
@@ -148,8 +183,8 @@ int Node_state_request(char * node_state_req, uint64_t node_id ){
 	uint8_t type = 0x7;
     //len = 26
  	uint8_t len = 0x8;
-	memcpy(node_hash, &type, 1);
-	memcpy(node_hash+1, &len, 1);
+	memcpy(node_state_req, &type, 1);
+	memcpy(node_state_req+1, &len, 1);
 	memcpy(node_state_req+2, &node_id, 8);
 
 	return len+TLV_HEADER;
@@ -160,7 +195,7 @@ int Node_state_request(char * node_state_req, uint64_t node_id ){
 //Création de Node State
 
 int Node_state(char * nodestate, uint64_t node_id, uint16_t seqno, char * node_hash, char * data, int data_length) {
-	memset(nodestate, 0, BUF_SIZE-MSG_HEADER);
+	memset(nodestate, 0, SIZE-MSG_HEADER);
 
 	uint8_t type = 0x8;
 	//len = 28 sans data length
@@ -184,9 +219,10 @@ int Warning(char * warning, char * message, int message_length) {
 	memcpy(warning, &type, 1);
 	memcpy(warning+1, &len, 1);
 	memcpy(warning+2, message, message_length);
-	return len+TLV_ENTETE;
+	return len+TLV_HEADER;
 }
 
 
 
 /****************************************************************************/
+
