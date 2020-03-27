7830c3e48a56f5fbf9d1631c8496197b2a02db00
MIRET blanche <blanchemiret@gmail.com>
2020-03-27 11:17:58 +0100

    Create tlv header

---
+++ tlv.h
@@ -0,0 +1,16 @@
+int main_datagram(char * msg);
+uint16_t get_body_length(char * message);
+int set_msg_body(char *message, char *body, uint16_t len);
+uint8_t getTLV_TYPE(char * tlv);
+uint8_t getTLV_LENGTH(char * tlv);
+char * getTLV_BODY(char * tlv);
+int Pad1(char * pad);
+int PadN(char * pad, uint8_t len);
+int Neighbour_request(char * neighbourReq);
+int Neighbour(char * neigbour, struct in6_addr IP, in_port_t port);
+int Network_hash(char * network_hash, char * hash);
+int Network_state_request(char * network_req);
+int Node_hash(char * node_hash, uint64_t node_id, uint16_t seqno, char * hash);
+int Node_state_request(char * node_state_req, uint64_t node_id );
+int Node_state(char * nodestate, uint64_t node_id, uint16_t seqno, char * node_hash,  char * data, size_t data_length);
+int Warning(char * warning, char * message, int message_length);
