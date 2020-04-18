dc9c5944d3668e91ca7e6e8b4ba0d6a6fa451e6e
CHEKROUN hiba-asmaa <chekrouh@informatique.univ-paris-diderot.fr>
2020-04-18 12:18:22 +0200

    update tlv header

--- tlv.h
+++ tlv.h
@@ -1,9 +1,17 @@
+#define MSG_HEADER 4
+#define TLV_HEADER 2
+
+#define SIZE 1024
+
+
+
 void* main_datagram();
 uint16_t get_body_length(char * message);
+int check_datagram_header(char * msg_received);
 int set_msg_body(char *message, char *body, uint16_t len);
-uint8_t getTLV_TYPE(char * tlv);
-uint8_t getTLV_LENGTH(char * tlv);
-char * getTLV_BODY(char * tlv);
+uint8_t get_tlv_type(char * tlv);
+uint8_t get_tlv_length(char * tlv);
+char * get_tlv_body(char * tlv);
 int Pad1(char * pad);
 int PadN(char * pad, uint8_t len);
 int Neighbour_request(char * neighbourReq);
@@ -15,3 +23,4 @@ int Node_state_request(char * node_state_req, uint64_t node_id );
 int Node_state(char * nodestate, uint64_t node_id, uint16_t seqno, char * node_hash,  char * data, size_t data_length);
 int Warning(char * warning, char * message, int message_length);
 void print_datagram(char *msg);
+
