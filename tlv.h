cfe27f6c963cf2e06363b911d9505c6139fa7c0f
MIRET blanche <blanchemiret@gmail.com>
2020-04-22 23:49:18 +0200

    Create tlv structures

--- tlv.h
+++ tlv.h
@@ -3,6 +3,82 @@
 
 #define SIZE 1024
 
+/*
+struct in6_addr {
+      unsigned char s6_addr[16];
+};
+*/
+
+
+
+struct neighbour_b { // <------------ SIZE = 18
+	struct in6_addr 	iPv6_addr;
+	uint16_t 			port; //<---- ici en réseau ou host ??
+}
+
+struct nethash_b { // <------------ SIZE = 16
+	char 		network_hash[16]; 
+}
+
+struct nodehash_b { // <------------- SIZE = 26
+	uint64_t	node_id;
+	uint16_t	seq_no;
+	char 		node_hash[16];
+}
+
+struct nodestatereq_b {
+	uint64_t	node_id;
+}
+
+struct nodestate_b { // <---------- SIZE = 26 + taille DATA
+	uint64_t  	node_id;
+	uint16_t  	seq_no;
+	char      	node_hash[16];
+	char      	data[192];
+};
+
+union tlv_body {
+	struct neighbour_b		neighbour_body;
+	struct nethash_b		nethash_body;
+	struct nodehash_b		nodehash_body;
+	struct nodestatereq_b	nodestatereq_body;
+	struct nodestate_b 		nodestate_body;
+	struct warning_b		warning_body;
+};
+  
+struct tlv_t {
+	int 			type; // <------- ENUM À CONSTRUIRE ?
+	int 			length; //pour Pad1, Neighbour Request, Network State Request : 0 / pour PadN : différent de zéro !
+	union tlv_body 	*body; //pour Pad1, PadN, Neighbour Request, Network State Request à NULL (pas de body)
+};
+
+struct dtg_t {
+	int 			magic;
+	int 			version;
+	uint16_t		body_length;
+	struct tlv_t 	*tlv_list;
+	int 			nb_tlv;
+}
+
+/*
+To do :
+- objet structure passé de fonction en fonction 
+
+Fonctions niveau supérieur <---- À COMPLÉTER
+- La donnée est-elle la même que chez nous ?
+- Le node hash est-il le même que le notre ? 
+- Ne pas oublier de mettre à jour les node hash, notre node hash, etc.
+
+Fonctions
+- create pour chaque type de tlv, qui renvoit un pointeur vers un tlv
+(- un fonction build tlv -> char )
+- une fonction build tlv + tlv + tlv... -> dtg -> char 
+
+- une fonction char -> dtg qui renvoit un pointeur vers un dtg
+--> ensuite switch sur tlv->type (ps, les tlv reçus peuvent se traiter indépendamment les uns des autres )
+
+*/
+
 
 
 void* main_datagram();
