db0ae622b13958009a21050a1cbd50fb00619b08
MIRET blanche <blanchemiret@gmail.com>
2020-04-24 12:44:54 +0200

    Create tlv structures + create and print function

---
+++ tlv_manager.c
@@ -0,0 +1,273 @@
+#include "tlv_manager.h"
+
+
+// ****************************************************************************
+// --------------------------- NVLLES FONCTIONS -------------------------------
+
+/*
+Fonctions
+- create pour chaque type de tlv, prend paramètres, renvoit un pointeur vers un struct tlv 
+(- un fonction build tlv -> char )
+- une fonction build_dtg qui prend tlv + tlv + tlv... -> dtg -> char 
+
+- une fonction char -> dtg qui renvoit un pointeur vers un dtg tout frais et liste chainée de struct tlv
+--> ensuite switch sur tlv->type (ps, les tlv reçus peuvent se traiter indépendamment les uns des autres )
+*/
+
+// ----------------------------------------------------------------------------
+// -------------------------- FONCTIONS CREATION TLV --------------------------
+
+// Fonction interne
+void *new_tlv() {
+	struct tlv_t *tlv = malloc(sizeof(struct tlv_t));
+	memset(tlv, 0, sizeof(struct tlv_t));
+	return tlv;
+}
+
+void *new_pad1() {
+	struct tlv_t *tlv = new_tlv();
+	tlv->type = 0;
+	tlv->length = 0;
+	return tlv;
+}
+
+void *new_padN(int nbzeros) {
+	struct tlv_t *tlv = new_tlv();
+	tlv->type = 1;
+	tlv->length = nbzeros;
+	return tlv;
+}
+
+/*
+
+	struct in6_addr {
+      	unsigned char s6_addr[16];
+	};
+
+  	struct sockaddr_in6 {
+      	sa_family_t sin6_family;
+      	in_port_t sin6_port;
+      	struct in6_addr sin6_addr;
+	};
+
+*/
+
+void *new_neighbour_request(){
+	struct tlv_t *tlv = new_tlv();
+	tlv->type = 2;
+	tlv->length = 0;
+	return tlv;
+}
+
+void *new_neighbour(struct in6_addr IP, in_port_t port) {
+	struct tlv_t *tlv = new_tlv();
+	tlv->type = 3;
+	tlv->length = 18; 
+
+	struct neighbour_b *neighbour_body = malloc(sizeof(struct neighbour_b));
+	memset(neighbour_body, 0, sizeof(struct neighbour_b));
+	neighbour_body->iPv6_addr = IP;
+	neighbour_body->port = port;
+
+	tlv->body.neighbour_body = neighbour_body;
+	return tlv;
+}
+
+void *new_network_hash(char network_hash[16]) {
+	struct tlv_t *tlv = new_tlv();
+	tlv->type = 4;
+	tlv->length = 16;
+
+	struct nethash_b *nethash_body = malloc(sizeof(struct nethash_b));
+	memset(nethash_body, 0, sizeof(struct nethash_b));
+	memcpy(nethash_body->network_hash, network_hash, 16);
+
+	tlv->body.nethash_body = nethash_body;
+	return tlv;
+}
+
+void *new_network_state_request() {
+	struct tlv_t *tlv = new_tlv();
+	tlv->type = 5;
+	tlv->length = 0;
+	return tlv;
+}
+
+void *new_node_hash(uint64_t node_id, uint16_t seqno, char nodehash[16]) {
+	struct tlv_t *tlv = new_tlv();
+	tlv->type = 6;
+	tlv->length = 26;
+
+	struct nodehash_b *nodehash_body = malloc(sizeof(struct nodehash_b));
+	memset(nodehash_body, 0, sizeof(struct nodehash_b));
+	nodehash_body->node_id = node_id;
+	nodehash_body->seq_no = seqno;
+	memcpy(nodehash_body->node_hash, nodehash, 16);
+
+	tlv->body.nodehash_body = nodehash_body;
+	return tlv;
+}
+
+void *new_node_state_request(uint64_t node_id) {
+	struct tlv_t *tlv = new_tlv();
+	tlv->type = 7;
+	tlv->length = 8;
+
+	struct nodestatereq_b *nodestatereq_body = malloc(sizeof(struct nodestatereq_b));
+	memset(nodestatereq_body, 0, sizeof(struct nodestatereq_b));
+	nodestatereq_body -> node_id = node_id;
+
+	tlv->body.nodestatereq_body = nodestatereq_body;
+	return tlv;
+}
+
+void *new_node_state(uint64_t node_id, uint16_t seq_no, char node_hash[16], char *data) {
+	if (strlen(data) > 192) exit(1);
+
+	struct tlv_t *tlv = new_tlv();
+	tlv->type = 8;
+	tlv->length = 26 + strlen(data);
+
+	struct nodestate_b *nodestate_body = malloc(sizeof(struct nodestate_b));
+	memset(nodestate_body, 0, sizeof(struct nodestate_b));
+	nodestate_body->node_id = node_id;
+	nodestate_body->seq_no = seq_no;
+	memcpy(nodestate_body->node_hash, node_hash, 16);
+	memcpy(nodestate_body->data, data, strlen(data));
+
+	tlv->body.nodestate_body = nodestate_body;
+	return tlv;
+}
+
+void *new_warning(char *message) {
+	struct tlv_t *tlv = new_tlv();
+	tlv->type = 9;
+	tlv->length = strlen(message);
+
+	struct warning_b *warning_body = malloc(sizeof(struct warning_b));
+	memset(warning_body, 0, sizeof(struct warning_b));
+	memcpy(warning_body->message, message, strlen(message));
+
+	tlv->body.warning_body = warning_body;
+	return tlv;
+}
+
+// ----------------------------------------------------------------------------
+// ----------------------- FONCTION CREATION DATAGRAMME -----------------------
+
+
+void *build_dtg(int nbtlv, ...) {
+	va_list valist;
+	int i;
+
+	va_start(valist, nbtlv);
+	return NULL;
+}
+
+// ----------------------------------------------------------------------------
+// --------------------------- FONCTION D'AFFICHAGHE --------------------------
+
+// #include <arpa/inet.h>
+
+//const char *inet_ntop(int af, const void *src,
+//                      char *dst, socklen_t cnt);
+//AF_INET6
+//src doit pointer sur une structure struct in6_addr 
+//(octets stockés dans l'ordre du réseau) qui est convertie dans la représentation 
+//la plus appropriée de l'adresse IPv6. 
+//Le tampon dst doit mesurer au minimum INET6_ADDRSTRLEN octets.
+
+void print_IP_addr(struct in6_addr *sin6_addr) {
+	char buf[INET6_ADDRSTRLEN];
+	inet_ntop(AF_INET6, sin6_addr, buf, INET6_ADDRSTRLEN);
+	printf("IP address is : %s\n", buf);
+}
+
+void print_hash(char hash[16]) { // <---- code de Dao je crois, à revoir...
+	for (int i = 0; i < 16; i++) {
+        printf("%02x", hash[i]);
+    }
+    printf("\n");
+}
+
+
+void print_node_id(uint64_t node_id) {
+	printf("Node id is : %"PRIu64"\n", node_id);
+}
+
+void print_seqno(uint16_t seqno) { // <---- stocké en format host dans le tlv 
+	printf("Seq num is : %"PRIu16, seqno);
+	printf("\n");
+}
+
+void print_tlv(struct tlv_t *tlv) {
+	printf(" -------------- TLV --------------\n");
+	printf("TLV type : %d\n", tlv->type);
+	printf("TLV length : %d\n", tlv->length);
+
+	switch(tlv->type) {
+		case 0:
+			printf("This is a Pad1, nothing to display.\n");
+			break;
+
+		case 1:
+			printf("This is a PadN, nothing to display.\n");
+			break;
+
+		case 2:
+			printf("This is a Neighbour request, nothing to display.\n");
+			break;
+
+		case 3:
+			printf("This is a Neighbour tlv.\n");
+			print_IP_addr(&(tlv->body.neighbour_body->iPv6_addr));
+			printf("Port is : %d\n", tlv->body.neighbour_body->port); // stocké en format host dans les tlv utilisés par le programme
+			break;
+
+		case 4:
+			printf("This is a Network Hash.\n");
+			printf("Network hash is : ");
+			print_hash(tlv->body.nethash_body->network_hash);
+			break;
+			
+		case 5:
+			printf("This is a Network State Request, nothing to display.\n");
+			break;
+
+		case 6:
+			printf("This is a Node Hash.\n");
+			print_node_id(tlv->body.nodehash_body->node_id);
+			print_seqno(tlv->body.nodestate_body->seq_no);
+			printf("Node hash is : ");
+			print_hash(tlv->body.nodehash_body->node_hash);
+			break;
+
+		case 7:
+			printf("This is a Node State Request.\n");
+			print_node_id(tlv->body.nodestatereq_body->node_id);
+			break;
+
+		case 8:
+			printf("This is a Node State.\n");
+			print_node_id(tlv->body.nodestate_body->node_id);
+			print_seqno(tlv->body.nodestate_body->seq_no);
+			printf("Node Hash is : ");
+			print_hash(tlv->body.nodestate_body->node_hash);
+			printf("Data is : %s\n", tlv->body.nodestate_body->data);
+			break;
+
+		case 9:
+			printf("This is a Warning.\n");
+			printf("Message is : %s\n", tlv->body.warning_body->message);
+			break;
+
+		default :
+			printf("This is not a TLV.\n");
+			exit(1);
+
+	}
+}
+
+
+
+
---
+++ tlv_manager.h
@@ -0,0 +1,124 @@
+#ifndef TLV_MANAGER_H
+#define TLV_MANAGER_H 
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <inttypes.h>
+#include <stdarg.h>
+#include <arpa/inet.h>
+
+
+#define MSG_HEADER 4
+#define TLV_HEADER 2
+
+#define SIZE 1024
+
+
+
+/*
+struct in6_addr {
+      unsigned char s6_addr[16];
+};
+*/
+
+
+
+struct neighbour_b { // <------------ SIZE = 18
+	struct in6_addr 	iPv6_addr;
+	in_port_t 			port; //<---- ici en réseau ou host ?? Spécification à définir au choix.
+};
+
+struct nethash_b { // <------------ SIZE = 16
+	char 		network_hash[16]; 
+};
+
+struct nodehash_b { // <------------- SIZE = 26
+	uint64_t	node_id;
+	uint16_t	seq_no;
+	char 		node_hash[16];
+};
+
+struct nodestatereq_b { // <--------- SIZE = 8
+	uint64_t	node_id;
+};
+
+struct nodestate_b { // <---------- SIZE = 26 + taille DATA
+	uint64_t  	node_id;
+	uint16_t  	seq_no;
+	char      	node_hash[16];
+	char      	data[192]; // <----------- à memset, et ainsi strlen fonctionne
+};
+
+struct warning_b {
+	char		message[SIZE - MSG_HEADER - TLV_HEADER];
+};
+
+union tlv_body {
+	struct neighbour_b		*neighbour_body;
+	struct nethash_b		*nethash_body;
+	struct nodehash_b		*nodehash_body;
+	struct nodestatereq_b	*nodestatereq_body;
+	struct nodestate_b 		*nodestate_body;
+	struct warning_b		*warning_body;
+};
+  
+struct tlv_t {
+	int 			type; // <------- ENUM À CONSTRUIRE ?
+	int 			length; //pour Pad1, Neighbour Request, Network State Request : 0 / pour PadN : différent de zéro !
+	union tlv_body 	body; //pour Pad1, PadN, Neighbour Request, Network State Request à NULL (pas de body)
+};
+
+struct dtg_t {
+	int 			magic;
+	int 			version;
+	uint16_t		body_length;
+	struct tlv_t 	*tlv_list;
+	int 			nb_tlv;
+};
+
+void *new_pad1();
+void *new_padN(int nbzeros);
+void *new_neighbour_request();
+void *new_neighbour(struct in6_addr IP, in_port_t port);
+void *new_network_hash(char network_hash[16]);
+void *new_network_state_request();
+void *new_node_hash(uint64_t node_id, uint16_t seqno, char nodehash[16]);
+void *new_node_state_request(uint64_t node_id);
+void *new_node_state(uint64_t node_id, uint16_t seq_no, char node_hash[16], char *data);
+void *new_warning(char *message);
+
+void print_tlv(struct tlv_t *tlv);
+
+
+
+/*
+To do :
+- objet structure passé de fonction en fonction 
+	- id-noeud
+	- num sequence
+	- donnée
+	- HASH du noeud <--- ne bouge pas
+	- HASH du réseau <---- à mettre à jour quand nécessaire 
+	- table des données <----- Hash à mettre à jour quand nécessaire
+	- table des voisins 
+
+Fonctions niveau supérieur <---- À COMPLÉTER
+- La donnée est-elle la même que chez nous ?
+- Reception node hash : le node hash est-il le même que le notre ? 
+- Ne pas oublier de mettre à jour les node hash, notre node hash, etc.
+
+Fonctions
+- create pour chaque type de tlv, prend paramètres, renvoit un pointeur vers un struct tlv 
+(- un fonction build tlv -> char )
+- une fonction build_dtg qui prend tlv + tlv + tlv... -> dtg -> char 
+
+- une fonction char -> dtg qui renvoit un pointeur vers un dtg tout frais et liste chainée de struct tlv
+--> ensuite switch sur tlv->type (ps, les tlv reçus peuvent se traiter indépendamment les uns des autres )
+
+*/
+
+#endif
