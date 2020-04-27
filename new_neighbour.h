9209796a42e7b88ef63d543553f4a1232a1a64c6
MIRET blanche <blanchemiret@gmail.com>
2020-04-27 23:17:20 +0200

    Write new_neighbour module

--- new_neighbour.h
+++ new_neighbour.h
@@ -1,11 +1,26 @@
 #ifndef __NEW_NEIGHBOUR_H__
 #define __NEW_NEIGHBOUR_H__
 
-int get_table_len(GHashTable *neighbour_table);
-int add_neighbour(GHashTable *neighbour_table, struct sockaddr *key, int perm);
-int update_last_reception(GHashTable *neighbour_table, struct sockaddr *key);
-int sweep_neighbour_table(GHashTable *neighbour_table);
-void display_neighbour_table(GHashTable *neighbour_table);
-void neighbour_table_iter(GHashTable *neighbour_table);
+#define NBMAX 15
+
+#include <sys/time.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct neighbour {
+		int					exists;
+		int 				permanent;
+		struct timeval		last_reception;
+		struct sockaddr		socket_addr;
+};
+
+int get_nb_neighbour(struct neighbour *neighbour_table);
+int add_neighbour(struct neighbour *neighbour_table, struct sockaddr *key, int perm);
+int find_neighbour(struct neighbour *neighbour_table, struct sockaddr *key);
+int update_last_reception(struct neighbour *neighbour_table, struct sockaddr *key);
+int sweep_neighbour_table(struct neighbour *neighbour_table);
+void display_neighbour_table(struct neighbour *neighbour_table);
 
 #endif
