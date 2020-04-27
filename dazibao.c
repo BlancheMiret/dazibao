8ffb8678d286a02a99255434b0c7c8026cb57ffc
MIRET blanche <blanchemiret@gmail.com>
2020-04-27 16:41:22 +0200

    Create nstate_t structure

--- dazibao.c
+++ dazibao.c
@@ -36,6 +36,17 @@ void print_hexa(char hash[16]) {
 }
 
 GHashTable *table_voisins;
+
+struct nstate_t {
+    uint64_t    node_id; // <-- quand même besoin de savoir quel est notre propre noeud au moment de la réponse à un TLV Node State
+    uint16_t    num_seq; // <-- stocké en format réseau |
+    char        *data[192]; //                          | --> en fait, pas nécessaire dans le node_state, mais à ajouter dans la table des données
+    char        node_hash[16]; // <--- ne bouge pas     | 
+    char        network_hash[16]; // <---- à mettre à jour quand nécessaire
+    GHashTable  *neighbour_table;
+    GHashTable  *data_table; // <---- hash des noeuds à mettre à jour quand nécessaire
+}
+
 int main (void) {
 
 
