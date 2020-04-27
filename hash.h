ac924e66c7a396e7823ced2af8bb7fc8f1510b1b
MIRET blanche <blanchemiret@gmail.com>
2020-04-27 18:02:56 +0200

    Update hash.h

--- hash.h
+++ hash.h
@@ -8,10 +8,11 @@ BESOINS
 FONCTIONS
  Peut - petre juste module de calcul de hash après tout
 
-- char[16] hash(char *to_hash); // <-- utilise SHA256 et tronque à 16 octets, les autres foncitons concatènent les données à hacher. Fonction interne
+- int hash(char *to_hash, char hash[16]); // <-- met à jour valeur à l'adresse hash
+	 // <-- utilise SHA256 et tronque à 16 octets, les autres foncitons concatènent les données à hacher. Fonction interne
 
-- char[16] hash_node(uint64_t node_id, uint16_t seq_no, char data[192]);
-- char[16] hash_network(nstate_t node_state);
+- int hash_node(uint64_t node_id, uint16_t seq_no, char data[192], char node_hash[16]); // <-- met à jour valeur à l'adresse node_hash
+- int hash_network(nstate_t node_state, char network_hash[16]); // <-- met à jour valeur à l'adresse network_hash
 
 */
 
