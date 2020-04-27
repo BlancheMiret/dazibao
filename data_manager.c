94287ff637e8f5e76a288c1361fa46342a7e2eb1
MIRET blanche <blanchemiret@gmail.com>
2020-04-27 18:03:24 +0200

    Add create_data_table and add_data functions

--- data_manager.c
+++ data_manager.c
@@ -1,8 +1,42 @@
 #include "data_manager.h"
+//#include "hash.h"
+
+/*
+struct data_t {
+	uint16_t	seq_no; // <-- STOCKÉ EN ORDRE HOST, PAS RÉSEAU !!!!!
+	char		data[192];
+	char		node_hash[16]; // <-- pas obligé de le recalculer à chaque fois tiens...
+};
+*/
+
 
 void *create_data_table() {
-	return g_hash_table_new_full(NULL, NULL);
+	return g_hash_table_new_full(NULL, NULL, free, free);
 }
+
+int get_data_table_len(GHashTable *data_table) { 
+	return g_hash_table_size(data_table);
+}
+
+// TRAVAIL EN COURS : BESOIN DU CALCUL DE NODE HASH !!
+int add_data(GHashTable *data_table, uint64_t node_id, uint16_t seq_no, char data[192]) { // <-- ATTENTION, SEQ_NO EN ORDRE HOST !!
+	// créer allocation pour node_id : key
+	uint64_t *key = malloc(sizeof(uint64_t));
+	mempcy(key, &node_id, 8);
+
+	// créer allocation 
+	struct data *value = malloc(sizeof(struct data_t));
+	memset(value, 0, sizeof(struct data_t));
+	memcpy(&value->seq_no, &seq_no, 2);
+	memcpy(value->data, data, 192);
+	// char node_hash[16];
+	// hash_node(node_id, seq_no, data, &node_hash); // <-------------------- inscrit le hash à l'adresse de node_hash 
+	// memcpy(valuer->node_hash, node_hash, 16);
+
+	g_hash_table_inser(data_table, key, value);
+}
+
+
 
 
 // Fonctions :
