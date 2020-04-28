4a90f95ef818134508339f22a562a421e06e598d
MIRET blanche <blanchemiret@gmail.com>
2020-04-28 12:21:32 +0200

    Add print_hash(), hash_node() and print_hash() to hash.c

--- hash.c
+++ hash.c
@@ -1,1 +1,48 @@
 #include "hash.h"
+
+void print_hash(char *hash) {
+    for (int i = 0; i < 16; i++) {
+        printf("%02x", hash[i]);
+    }
+    printf("\n");
+}
+
+
+int hash(char *value_to_hash, size_t size_value_to_hash, char *final_hash) {
+	unsigned char *res = SHA256((const unsigned char*)value_to_hash, size_value_to_hash, 0);
+	memcpy(final_hash, res, 16);
+	return 0;
+}
+
+
+/* seqno en format RÉSEAU DANS LES PARAMÈTRES*/
+int hash_node(uint64_t node_id, uint16_t seq_no, char data[192], char *final_hash) {
+	int TRIPLETSIZE = sizeof(uint64_t) + sizeof(uint16_t) + strlen(data);
+	char triplet[TRIPLETSIZE];
+	memcpy(triplet, &node_id, 8);
+	memcpy(triplet+8, &seq_no, 2); //selon le sujet page 2, doit bien être en big-endian dans le hash
+    memcpy(triplet+10, data, strlen(data));
+    hash(triplet, TRIPLETSIZE, final_hash); // <--- hachage
+    return 0;
+}
+
+/*
+int hash_network(GHashTable *data_table, char *final_hash) {
+	int nb_data = g_hash_table_size(data_table);
+	int CONCATSIZE = nb_data * 16; // <-- 16 = taille d'un node_hash
+	char hash_concat[CONCATSIZE];
+
+	GHashTableIter iter;
+	gpointer key, value;
+
+	g_hash_table_iter_init (&iter, data_table);
+	int i = 0;
+	while (g_hash_table_iter_next (&iter, &key, &value)) {
+		memcpy(hash_concat + i, ((struct data_t*)value)->node_hash, 16);
+		i++;
+    }
+
+	hash(hash_concat, CONCATSIZE, final_hash);
+	return 0;
+}
+*/
--- hash.h
+++ hash.h
@@ -1,19 +1,12 @@
 #ifndef __HASH_H__
 #define __HASH_H__
 
-/*
-BESOINS
-- Calculer un hash
+#include <openssl/sha.h>
+//#include <glib.h>
+//#include <glib/gprintf.h>
 
-FONCTIONS
- Peut - petre juste module de calcul de hash après tout
-
-- int hash(char *to_hash, char hash[16]); // <-- met à jour valeur à l'adresse hash
-	 // <-- utilise SHA256 et tronque à 16 octets, les autres foncitons concatènent les données à hacher. Fonction interne
-
-- int hash_node(uint64_t node_id, uint16_t seq_no, char data[192], char node_hash[16]); // <-- met à jour valeur à l'adresse node_hash
-- int hash_network(nstate_t node_state, char network_hash[16]); // <-- met à jour valeur à l'adresse network_hash
-
-*/
+int hash_node(uint64_t node_id, uint16_t seq_no, char data[192], char *final_hash);
+//int hash_network(GHashTable *data_table, char *final_hash);
+void print_hash(char *hash);
 
 #endif
