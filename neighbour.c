57d735ee1b8dadbe11dbd2ab4449f4f31da35f35
MIRET blanche <blanchemiret@gmail.com>
2020-04-28 12:22:12 +0200

    Switch struct neighbour definition from neighbour.c to neighbour.h

--- neighbour.c
+++ neighbour.c
@@ -13,11 +13,6 @@
 --> VOIR G_HASH_TABLE_FULL, SINON FUITE DE MÉMOIRE
 */
 
-struct neighbour {
-		int 				permanent;
-		struct timeval		last_reception;
-};
-
 // Renvoie un pointeur vers une nouvelle hashtable
 void* create_neigh_table() {
 	return g_hash_table_new(NULL, NULL);
