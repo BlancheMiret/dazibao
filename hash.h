d3003f4b03c0165b3402244091a33e3c1246c3a2
MIRET blanche <blanchemiret@gmail.com>
2020-05-04 12:30:22 +0200

    Add compare_2_hash function

--- hash.h
+++ hash.h
@@ -7,6 +7,7 @@
 //#include <glib.h>
 //#include <glib/gprintf.h>
 
+int compare_2_hash(char hash1[16], char hash2[16]);
 int hash(char *value_to_hash, size_t size_value_to_hash, char *final_hash);
 int hash_node(uint64_t node_id, uint16_t seq_no, char data[192], char *final_hash);
 void print_hash(char *hash);
