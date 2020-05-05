627cd932e8bf6f1624be2baae234e3994c61f35b
MIRET blanche <blanchemiret@gmail.com>
2020-05-05 16:20:00 +0200

    Correct data type

--- peer_state.h
+++ peer_state.h
@@ -8,7 +8,7 @@
 struct pstate_t {
 	uint64_t            node_id; // <-- quand même besoin de savoir quel est notre propre noeud au moment de la réponse à un TLV Node State
 	uint16_t            num_seq; // <-- stocké en format réseau |
-	char                *data[192]; //                          | --> en fait, pas nécessaire dans le node_state, mais à ajouter dans la table des données
+	char                data[192]; //                          | --> en fait, pas nécessaire dans le node_state, mais à ajouter dans la table des données
 	char                node_hash[16]; // <--- ne bouge pas     | 
 	char                network_hash[16]; // <---- à mettre à jour quand nécessaire
 	struct neighbour    neighbour_table[15];
