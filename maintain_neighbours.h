d4e4c1958f271a923c7a6828b1b3ca6e81e1eb36
chekrouh <chekrouh@informatique.univ-paris-diderot.fr>
2020-05-14 11:48:51 +0200

    update maintain_neighbours.h

--- maintain_neighbours.h
+++ maintain_neighbours.h
@@ -18,8 +18,9 @@
 #include "peer_state.h"
 #include "tlv_manager.h"
 
-void send_neighbour_req(int socket, struct pstate_t * peer_state);
+
 int compare_addr(struct in6_addr *IP1, struct in6_addr *IP2);
-void maintain_neighbour_table(struct pstate_t * peer_state, struct sockaddr_in6 from);
-void send_network_hash(int socket, struct pstate_t * peer_state);
+void update_neighbour_table(struct pstate_t * peer_state, struct sockaddr_in6 from);
+void send_neighbour_req(int socket, struct pstate_t * peer_state);
+int send_network_hash(int socket, struct pstate_t * peer_state);
 
